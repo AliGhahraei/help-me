20c1f20acb4d162835384eaebcf3bdc67dc7e458
Ali Ghahraei <aligf94@gmail.com>
2017-08-26 20:20:40 -0500

    Add static files and user urls

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    url('^/', auth_views.LoginView.as_view(template_name='users/login.html')),
+    
+]
